7d7f50eb6b3f682f1207c44910d7d4c426e857a3
xiaozhang <zhenpsm@163.com>
2020-10-19 18:49:28 +0800

    前端网络请求

---
+++ src/template/src/utils/request.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+
+let axiosObj = axios.create();
+
+export const get = (url, val, config = {}) => {
+    return myAxios.get(url, {
+        params: val,
+        ...config,
+        data: {customParams: config.customParams},
+    });
+};
+// 删除公用方法
+export const del = (url, data, config = {}) => {
+    return myAxios.delete(url, {
+        data: config.customParams
+            ? {...data, customParams: config.customParams}
+            : data,
+        ...config,
+    });
+};
+
+export const post = (url, val, config = {}) => {
+    let contentType;
+
+    return myAxios.request({
+        url,
+        data: config.customParams
+            ? {...val, customParams: config.customParams}
+            : val,
+        method: 'post',
+        headers: {
+            'Content-type': contentType,
+        },
+        ...config,
+    });
+};
+
+// 修改数据公用方法
+export const put = (url, val, config = {}) => {
+    let contentType;
+
+    return myAxios.request({
+        url,
+        data: val,
+        method: 'put',
+        headers: {
+            'Content-type': contentType,
+        },
+        ...config,
+    });
+};
+
+// formadata post 提交数据
+export const postFormData = (url, val, config = {}) => {
+    return myAxios.request({
+        url,
+        data: val,
+        method: 'post',
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+        },
+        ...config,
+    });
+};
+
+export default axiosObj;
